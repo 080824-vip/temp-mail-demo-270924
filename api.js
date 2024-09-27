ab7a7be55e65a162db162c6154dcec30773b53d5
Your Name <you@example.com>
2024-09-27 16:15:04 +0000

    Add additional logging to check domain reading and dropdown population

--- api.js
+++ api.js
@@ -48,6 +48,7 @@ function populateDomainSelect() {
     console.log('Domain select element:', domainSelect);
     console.log('Domain select element:', domainSelect);
     console.log('Domain select element:', domainSelect);
+    console.log('Domain select element:', domainSelect);
     console.log('Populating domain select with domains:', domains);
     domains.forEach(domain => {
         const option = document.createElement('option');
